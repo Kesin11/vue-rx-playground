a01b80b2c9d68efe3d1e8185570c3c76c27b3e7f
Kenta Kase <kesin1202000@gmail.com>
2017-08-04 01:57:02 +0900

    いいねボタン実装

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <ul>
       <li v-for="user in users">
-        {{ user.name }}
+        <div>{{ user.name }}</div>
+        <div>Like: {{ user.like }}</div>
+        <div>Very like: {{ user.very_like }}</div>
+        <button v-on:click="countLike(user)">いいね</button>
       </li>
     </ul>
 
@@ -14,34 +17,50 @@
 
 <script>
 import Rx from 'rxjs/Rx'
-const subject = new Rx.Subject()
-subject
+const counterSubject = new Rx.Subject()
+counterSubject
   .map(value => value + 10)
   .subscribe(value => {
     console.log(value)
   })
+
+// 一度subjectをnewしてからじゃないとsubject.mapできないのがイマイチなので使い方間違えてる気がしないでもないのだが・・・
+// Observableは最初に流す値が決まってる類しかなかったので、next()だけで制御するにはこれしか思いつかなかった
+const subject = new Rx.Subject()
+const likeSubject = subject.map(payload => {
+  const like = payload.user.like + payload.count
+  return Object.assign({}, payload.user, { like })
+})
 
 export default {
   name: 'app',
   data () {
     return {
       users: [
-        { id: 1, name: 'Ken'},
-        { id: 2, name: 'Alice'},
-        { id: 3, name: 'Van'},
+        { id: 1, name: 'Ken',   like: 0, very_like: 0 },
+        { id: 2, name: 'Alice', like: 0, very_like: 0 },
+        { id: 3, name: 'Van',   like: 0, very_like: 0 },
       ],
       count: 0,
     }
   },
   mounted: function() {
-    subject.subscribe( value => {
+    counterSubject.subscribe( value => {
       this.$data.count += value
+    })
+    likeSubject.subscribe( user => {
+      const user_id = user.id
+      const i = this.$data.users.findIndex((user) => user.id == user_id)
+      Object.assign(this.$data.users[i], user)
     })
   },
   methods: {
     countUp: function() {
-      subject.next(1)
+      counterSubject.next(1)
     },
+    countLike: function(user) {
+      likeSubject.next({ user: user, count: 10 })
+    }
   }
 }
 </script>
