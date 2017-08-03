df5e3deebc6eeeafb5e8c66ebbb53c382a80563f
Kenta Kase <kesin1202000@gmail.com>
2017-08-04 00:31:57 +0900

    簡単なカウンタを作るところまで

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="user in users">
+        {{ user.name }}
+      </li>
+    </ul>
+
+    <br>
+    <div>count: {{ count }}</div>
+    <button v-on:click="countUp">subscribeCount</button>
   </div>
 </template>
 
 <script>
+import Rx from 'rxjs/Rx'
+const subject = new Rx.Subject()
+subject
+  .map(value => value + 10)
+  .subscribe(value => {
+    console.log(value)
+  })
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      users: [
+        { id: 1, name: 'Ken'},
+        { id: 2, name: 'Alice'},
+        { id: 3, name: 'Van'},
+      ],
+      count: 0,
     }
+  },
+  mounted: function() {
+    subject.subscribe( value => {
+      this.$data.count += value
+    })
+  },
+  methods: {
+    countUp: function() {
+      subject.next(1)
+    },
   }
 }
 </script>
